aabd3a01d2e8296e8d597ee8b12e0fb8b5dc10dc
Zamzama <140917364+zamzama786@users.noreply.github.com>
2023-12-27 21:13:54 +0100

    commit initial files

---
+++ script.js
@@ -0,0 +1,21 @@
+// 1. grab the save-el paraghraph and store it in a variable
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+
+console.log(saveEl)
+
+function increment() {
+    count = count + 1
+    countEl.textContent = count
+}
+
+function save () {
+    let countStr = count + " -"
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+    console.log(count)
+}
+
